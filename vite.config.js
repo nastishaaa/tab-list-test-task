c6a013834314a2e3aee2eabe458b56815a5bfaa6
Anastasiia <anastasiiprfnk@gmail.com>
2025-06-24 17:39:59 +0300

    fix configs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  }
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    historyApiFallback: true, // ⬅️ ось це важливо
+  }
 });
